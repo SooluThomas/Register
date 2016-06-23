6db301f6e7c0b82632c3102bf2b0d619715be391
SooluThomas <soolu.elto@gmail.com>
2016-06-23 15:58:17 -0700

    Update 1.01

--- app/src/main/java/com/lmntrx/register/Main3Activity.java
+++ app/src/main/java/com/lmntrx/register/Main3Activity.java
@@ -1,7 +1,10 @@
 package com.lmntrx.register;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main3Activity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Main3Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase("UsersDB", MODE_PRIVATE,null);
+        String select = "Select * from USER;";
+        Cursor cursor = sqLiteDatabase.rawQuery(select, null);
+        if (cursor.getCount()>0){
+            cursor.moveToFirst();
+            do{
+                textView.setText(textView.getText() + "\n" + cursor.getString(1));
+            }while(cursor.moveToNext());
+        }
     }
 }
